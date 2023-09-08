104f3deb350a9d80d27f3fa0a19e56c9fc3053ad
frottobaggings <kobbybright78@gmail.com>
2023-09-07 18:45:54 -0700

    Commit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char text[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
