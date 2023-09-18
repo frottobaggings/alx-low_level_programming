878722ef1de8e808bcba96723779b9008a346e1a
frottobaggings <kobbybright78@gmail.com>
2023-09-18 15:16:36 -0700

    commit wai

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * @a: param
+ * @b: param1
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
